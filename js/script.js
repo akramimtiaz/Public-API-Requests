1ab811df17bc01bfabb3ac08a8bae6d28a601b54
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-05-01 18:21:40 +1000

    Add Search Functionality
    - added  createSearch() function which creates search elements
    - added configSearchHandler() function which sets up event listeners for both the search input box as well as the submit button
    - add search() function which filters the profiles to those matching the search criteria
    - refactored  configModalHandler() function to utilise DOM event bubbling
    - removed internal strong tags from within X button

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ const generateModal = (users) => {
         const modalBox = `
         <div class="modal-container">
         <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <button type="button" id="modal-close-btn" class="modal-close-btn">X</button>
             <div class="modal-info-container">
                 <img class="modal-img" src="${user.picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
@@ -111,72 +111,86 @@ const toggleModal = (currModal, toggle) => {
 
     const userEmail = currModal.querySelector('p').textContent;
     const cards = document.querySelectorAll('.card');
-    console.log('hello');
+  
     cards.forEach(card => {
         const cardEmail = card.querySelector('p').textContent;
         if(cardEmail === userEmail){
-            if(toggle === 'previous' && card.previousElementSibling !== null){
-                console.log();
+            if(toggle === 'modal-prev' && card.previousElementSibling !== null){
                 displayModal(card.previousElementSibling);
-            }else if (toggle === 'next' && card.nextElementSibling !== null){
+            }else if (toggle === 'modal-next' && card.nextElementSibling !== null){
                 displayModal(card.nextElementSibling);
             }
         }
     });
 
-   
-
 }
 
 const configModalHandler = () => {
 
-    const modals = document.querySelectorAll('.modal-container');
+        const modals = document.querySelectorAll('.modal-container');
 
-    modals.forEach(modal => {
-        const previousModalBtn = modal.querySelector('.modal-btn-container #modal-prev');
-        const nextModalBtn = modal.querySelector('.modal-btn-container #modal-next');
-
-        previousModalBtn.addEventListener('click', (event) => {
-            toggleModal(event.target.offsetParent, 'previous');
-        });
-    });
-
+        modals.forEach(modal => modal.addEventListener('click', (event) => {
+            console.log(event.target);
+            if(event.target.tagName === 'BUTTON'){
+                if(event.target.id === 'modal-prev' || event.target.id === 'modal-next'){
+                    toggleModal(event.target.offsetParent, event.target.id);
+                } else if (event.target.id === 'modal-close-btn'){
+                    event.currentTarget.style.display = 'none';
+                }
+            }
+       }));
 
 }
 
 
+const searchProfiles = (search) => {
 
+    const cards = document.querySelectorAll('.card');
 
+    cards.forEach(card => {
+        const profileName = card.querySelector('h3').textContent;
+        const profileEmail = card.querySelector('p').textContent;
+        
+        if(profileName.startsWith(search) || profileEmail.startsWith(search)){
+            card.style.display = '';
+        }else {
+            card.style.display = 'none';
+        }
+    });
+};
 
+const createSearch = () => {
 
+    const searchContainer = document.querySelector('.search-container');
+    const searchHTML = `
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+    </form>`;
+    
+    searchContainer.innerHTML += searchHTML;
+};
 
+const configSearchHandler = () => {
 
+    const search = document.querySelector('#search-input');
+    search.addEventListener('input', (event) => {
+        searchProfiles(event.target.value);
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const searchBtn = document.querySelector('#serach-submit');
+    searchBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        searchProfiles(document.querySelector('#search-input').textContent);
+    });
+};
 
 
 fetchData('https://randomuser.me/api/?nat=us&results=12')
         .then(data => generateGallery(data))
         .then(data => generateModal(data))
+        .then(createSearch)
         .then(configGalleryHandler)
-        .then(configModalHandler);
+        .then(configModalHandler)
+        .then(configSearchHandler);
    
